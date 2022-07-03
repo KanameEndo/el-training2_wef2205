851772864ab3fc55a07e093c0c32bf7c63b6155e
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-07-03 09:24:45 +0900

    最終チェック

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -26,10 +26,10 @@ RSpec.describe 'タスク管理機能', type: :system do
     context 'タイトルのあいまい検索とステータス検索をした場合' do
       it "検索キーワードをタイトルに含み、かつステータスに完全一致するタスク絞り込まれる" do
         visit tasks_path
-        fill_in 'task_name', with: 'タスク'
+        fill_in 'task_name', with: '内容'
         select "未着手", from: "task[status]"
         click_on '検索'
-        expect(page).to have_content 'タスク'
+        expect(page).to have_content '内容'
         expect(page).to have_content '未着手'
       end
     end
