6cd1abe25b2d2c409416abad256be5440aec6c67
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-07-03 21:01:19 +0900

    モデルspec完成

--- app/models/task.rb
+++ app/models/task.rb
@@ -8,6 +8,6 @@ class Task < ApplicationRecord
   enum status:{ 未着手: 0, 進行中: 1, 完了: 2 }
   enum priority: { 高: 0, 中: 1, 低: 2 }
 
-  scope :name_search, -> (task_name) {where('(name LIKE ?)',"%#{params[:task][:name]}%")}
-  scope :status_search, -> (task_status){where(status: params[:status][:status])}
+  scope :name_search, -> (name) {where("name LIKE ?", "%#{name}%")}
+  scope :status_search, -> (status) {where(status: status)}
 end
--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,25 +1,25 @@
 FactoryBot.define do
 
   factory :task do
-    name { 'タスク名１' }
-    content { '内容1' }
+    name { 'name1' }
+    content { 'content1' }
     deadline { DateTime.now }
     status {'完了'}
     priority {'高'}
   end
 
   factory :second_task, class: Task do
-    name { 'タスク名2' }
-    content { '内容2' }
-    deadline { DateTime.now }
+    name { 'name2' }
+    content { 'content2' }
+    deadline { DateTime.now +5 }
     status {'進行中'}
     priority {'中'}
   end
 
   factory :third_task, class: Task do
-    name { 'タスク名3' }
-    content { '内容3' }
-    deadline { DateTime.now }
+    name { 'name3' }
+    content { 'content3' }
+    deadline { DateTime.now +5 }
     status {'未着手'}
     priority {'低'}
   end
