82e55a6262078e85bd5c0fcba9720cf7045a6c9d
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-07-05 08:44:07 +0900

    ユーザのemailにユニーク制約設定

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_02_062142) do
+ActiveRecord::Schema.define(version: 2022_07_04_231149) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2022_07_02_062142) do
     t.boolean "admin"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "task_labels", "tasks"
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,9 @@
-50.times do |n|
-  name = Faker::Name.name
-  content = "Hogehoge"
-  created_at = "Hogehoge"
-  updated_at = "Hogehoge"
-  deadline = "Hogehoge"
-  status = "完了"
-  priority = "中"
-  Task.create!(name: name,
-              content: content,
-              created_at: created_at,
-              updated_at: updated_at,
-              deadline: deadline,
-              status: status,
-              priority: priority,
+10.times do |n|
+  name = Faker::Games::Pokemon.name
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(name: name,
+              email: email,
+              password: password,
               )
 end
