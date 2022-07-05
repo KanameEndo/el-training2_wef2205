7a02c29fc0dbca8fdc7449b2f7ab5d8414c23908
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-07-05 16:45:57 +0900

    管理画面のindex表示作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,41 +1,31 @@
 class UsersController < ApplicationController
-  before_action :admin_user
-  before_action :set_user, only: %i[ show edit update destroy ]
   skip_before_action :login_required, only: %i[ new create ]
 
-  # GET /users or /users.json
   def index
-    @users = User.all.order("created_at DESC")
+    @users = User.all
   end
 
-  # GET /users/1 or /users/1.json
   def show
   end
 
-  # GET /users/new
   def new
-    @user = User.new
+    @user = User.new(user_params)
   end
 
-  # GET /users/1/edit
   def edit
   end
 
-  # POST /users or /users.json
   def create
     @user = User.new(user_params)
 
-    respond_to do |format|
-      if @user.save
-        session[:user_id] = @user.id
-        format.html { redirect_to user_url(@user), notice: "User was successfully created." }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-      end
+    if @user.save
+      session[:user_id] = @user.id
+      format.html { redirect_to user_url(@user), notice: "User was successfully created." }
+    else
+      format.html { render :new, status: :unprocessable_entity }
     end
   end
 
-  # PATCH/PUT /users/1 or /users/1.json
   def update
     respond_to do |format|
       if @user.update(user_params)
@@ -46,26 +36,25 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1 or /users/1.json
+
   def destroy
     @user.destroy
 
     respond_to do |format|
       format.html { redirect_to users_url, notice: "User was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
   private
-    def set_user
-      @user = User.find(params[:id])
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    def admin_user
-      redirect_to(root_path) unless current_user.admin?
-    end
+  def admin_user
+    redirect_to(root_path) unless current_user.admin?
+  end
 
-    def user_params
-      params.require(:user).permit(:name, :email, :password, :password_digest, :password_confirmation, :admin)
-    end
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_digest, :password_confirmation, :admin)
+  end
 end
