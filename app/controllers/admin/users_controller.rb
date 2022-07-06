8fa0242655ab552a5571b82d784bb89851f17552
Kaname_Endo <uchiyama.uchiyama.travail@gmail.com>
2022-07-06 14:30:40 +0900

    コールバック削除作成

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,10 +1,64 @@
 class Admin::UsersController < ApplicationController
 
+  #ログインしてるユーザーと権限を持っているユーザーが同じかチェック
+  before_action :admin_check
+  before_action :set_user, only: %i[ show edit update destroy]
+
   def index
-    @users = User.all
+    @user = User.select(:id, :name, :email, :admin).order("created_at: :desc").page(params[:page]).per(7)
+    #ユーザーのid、名前、メール、権限、を選んで(select)表示。登録順に表示。
   end
 
   def show
-    @users = User.find(params[id])
+    @tasks = @user.tasks.all
+    @tasks = @tasks.page(params[:page]).per(7)
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+  end
+
+  def create
+  @user = User.new(user_params)
+  #送信されてきた値
+  if @user.save
+    #登録を押したとき
+    redirect_to admin_users_path, notice: "ユーザーの登録が完了しました"
+  else
+    render :new
+  end
+
+  def update
+    if @user.update(user_params)
+      #編集を押したとき
+      redirect_to admin_users_path, notice: "ユーザーを更新しました"
+    end
+      render :edit
+  end
+
+  def destroy_action
+    if @user.destroy
+      #
+      redirect_to admin_users_path, notice:"{@user.name}を削除しました"
+    else
+      redirect_to admin_users_path, notice:"管理者が存在しなくなるため削除できません"
+    end
+  end
+
+
+private
+def user_params
+  params.require(:user).permit(:name, :email, :password, :password_confirmation, :admin)
+end
+def set_user
+  @user = User.find(params[:id])
+end
+def admin_check
+  unless current_user && current_user.admin?
+    #true/falseを返す。==は同じなだけ
+    redirect_to root_path, notice: "アクセス不可です"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  has_many :tasks
+  before_destroy :cannot_destroy_only_one_admin
+  before_update :cannot_update_only_one_admin
+  has_many :tasks, dependent: :destroy
 
   has_secure_password
 
@@ -7,4 +9,24 @@ class User < ApplicationRecord
   validates :name, presence: true, length: { maximum: 30 }
   validates :email,uniqueness: true, presence: true, length: { maximum: 255 },
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+
+
+  private
+
+  def cannot_destroy_only_one_admin
+    @admin_users = User.where(admin: true)
+    if @admin_user.count == 1 && self.admin
+      errors.add :base, '少なくとも1人、ログイン用の認証が必要です'
+      # return falseではなく throw :abort
+      throw :abort
+    end
+  end
+
+  def cannot_update_only_one_admin
+    @admin_users = User.where(admin: true)
+    if @admin_user.count == 1 && self.admin
+      errors.add :base, '少なくとも1人、ログイン用の認証が必要です'
+      # return falseではなく throw :abort
+      throw :abort
+    end
 end
